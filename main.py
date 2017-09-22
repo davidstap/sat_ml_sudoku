a9eaf1f433c3e1c619e0bc0806df49462c612b5c
Joris <jsbaan@gmail.com>
2017-09-22 07:06:04 +0200

    terminal output processing v1

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import subprocess
+
+def clean_output(output):
+    clean_output = []
+    output = [a.split() for a in out.split('*'*80)]
+    for line in output:
+        statistics = []
+        for item in line:
+            try:
+                statistics.append(float(item))
+            except ValueError:
+                continue
+        if len(statistics) == 21:
+            del statistics[10]
+        elif len(statistics) == 22:
+            del statistics[10]
+            del statistics[10]
+        clean_output.append(statistics)
+    column_names = ["seconds", "level", "variables", "used", "original", "conflicts","learned","limit","agility", "mb"]
+    return column_names, clean_output
+
+
+# run sudoku and catch terminal output
 proc = subprocess.Popen(["python", "-c", "import sudoku; sudoku"], stdout=subprocess.PIPE)
-out = proc.communicate()[0]
-print(out.upper())
+out = str(proc.communicate()[0])
+column_names, clean_output = clean_output(out)
+print(len(column_names), len(clean_output))
